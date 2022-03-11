fc64c74be548dca01346268e454d0cf29e78eef1
panvla <pandvlado@gmail.com>
2022-03-11 23:37:52 +0100

    adding recipeform

--- src/main/java/com/vladimirpandurov/spring5recipeapp/contorllers/RecipeController.java
+++ src/main/java/com/vladimirpandurov/spring5recipeapp/contorllers/RecipeController.java
@@ -1,10 +1,10 @@
 package com.vladimirpandurov.spring5recipeapp.contorllers;
 
+import com.vladimirpandurov.spring5recipeapp.commands.RecipeCommand;
 import com.vladimirpandurov.spring5recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class RecipeController {
@@ -22,6 +22,19 @@ public class RecipeController {
 
         return "recipe/show";
 
+    }
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 
 }
